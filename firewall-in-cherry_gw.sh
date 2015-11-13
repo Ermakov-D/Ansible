5f82075b3a2c3b95da426991e4e135d36683a452
Dmitriy Ermakov <ermakov.dmitriy@gmail.com>
2015-11-13 09:46:46 +0300

    Изменения для закрепления: 	modified:   cherry-gw.yml 	modified:   firewall-in-cherry_gw.sh

--- firewall-in-cherry_gw.sh
+++ firewall-in-cherry_gw.sh
@@ -5,15 +5,19 @@ ip_address=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f
 
 if [ "$ip_address" = "192.168.15.51" ]
 then
-  remote_md5=`ssh depeche@cherry-gw "md5sum /var/lib/firewall" | awk '{print $1}'`
-  local_md5=`md5sum /srv/ansible-files/cherry-gw/firewall/firewall | awk '{print $1}'`
+  # remote_md5=`ssh depeche@cherry-gw "md5sum /var/lib/firewall" | awk '{print $1}'`
+  # local_md5=`md5sum /srv/ansible-files/cherry-gw/firewall/firewall | awk '{print $1}'`
 
 
   echo "remote: $remote_md5"
   echo " local: ${local_md5}"
 
-  if [ "$remote_md5" != "$local_md5" ]
-  then
+#  if [ "$remote_md5" != "$local_md5" ]
+#  then
+
+#    fi
+
     ansible-playbook cherry-gw.yml
-    fi
+
+
 fi
